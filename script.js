71433b9f97e2642562d6ae4c8b7ac361b39dca92
MFarel-dev <jalannyapu@gmail.com>
2025-10-20 08:43:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const tombolSapa = document.getElementById('sapaButton'):
-tombolSapa.addEventListener('click', function() {
-  alert('Button clicked!');
-});
+const tombolSapa = document.getElementById('sapaButton');
+
+if (tombolSapa) {
+  tombolSapa.addEventListener('click', function() {
+    alert('Tombol sapa telah diklik!');
+  });
+} else {
+  console.error('Elemen dengan ID "sapaButton" tidak ditemukan.');
+}
