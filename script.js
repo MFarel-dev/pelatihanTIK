349bc88b4242d83b27a5c7a7d6433fe4955d62db
MFarel-dev <jalannyapu@gmail.com>
2025-10-20 07:08:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton'):
 tombolSapa.addEventListener('click', function() {
-  alert('Halo Terima Kasih sudah berkunjung!');
-        });
+  alert('Button clicked!');
+});
